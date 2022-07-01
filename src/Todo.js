6ef215bebe20b2a391a930190d04f9f567502cb6
Steven Kao <stevenkaoo@gmail.com>
2022-07-02 09:31:36 +1000

    Update Todo and TodoApp.

--- src/Todo.js
+++ src/Todo.js
@@ -8,7 +8,6 @@ import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
-import uuid from "uuid/v4"
 
 function Todo({ task, completed, removeTodo, id, toggleTodo, editTodo }) {
   const [isEditing, toggle] = useToggleState(false);
--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useEffect } from "react";
 import useTodoState from "./hooks/useTodoState";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
